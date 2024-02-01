d53a01bd667d2026efe70654cd405fd72517a831
BallZzi <6431503102@lamduan.mfu.ac.th>
2024-02-01 15:11:50 +0700

    center contact

--- components/contact.vue
+++ components/contact.vue
@@ -1,27 +1,15 @@
 <template>
-<div class="align-center">
-
-  <v-card
-    class="mx-auto my-8"
-    max-width="344"
-    elevation="16"
-    
-  >
-    <v-card-item>
-      <v-card-title>
-        Integrated AgriTech Ecosystem Research(IATE)
-      </v-card-title>
-      
-    </v-card-item>
-<div class="text-brown">
-    <v-card-text >
-       333 Moo1, Thasud, Muang Chiang Rai 57100
-Tel: +66 5391 6741-2
-E-mail: sujita.arw@mfu.ac.th
-    </v-card-text>
-</div>
-   
+  <v-card max-width="344" max-height="190" elevation="16">
+    <div class="text-h5 text-brown mt-3 ml-4 text-center">
+      <h5>Integrated AgriTech Ecosystem Research(IATE)</h5>
+    </div>
+    <div class="text-brown">
+      <v-card-text class="text-center">
+        333 Moo1, Thasud, Muang Chiang Rai, <br />
+        Chiang Rai 57100 <br />
+        Tel: +66 5391 6741-2 <br />
+        E-mail: sujita.arw@mfu.ac.th
+      </v-card-text>
+    </div>
   </v-card>
-</div>
-
 </template>
