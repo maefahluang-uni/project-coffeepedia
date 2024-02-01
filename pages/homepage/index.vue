1264a8a491d2810162361e432271b6a285cdae20
BallZzi <6431503102@lamduan.mfu.ac.th>
2024-02-01 11:00:27 +0700

    adjust home as component

--- pages/homepage/index.vue
+++ pages/homepage/index.vue
@@ -1,69 +1,27 @@
 <template>
-  <!-- Removed container and head tag-->
-  <v-parallax src="@/assets/coffeeHome.jpg">
-    <v-divider color="brown"> </v-divider>
-    <v-card>
-      <v-tabs v-model="tab" color="rgb(240, 200, 165)" align-tabs="center">
-        <v-tab :value="1">Home</v-tab>
-        <v-tab :value="2">Type coffee beans</v-tab>
-        <v-tab :value="3">Blog</v-tab>
-        <v-tab :value="4">News</v-tab>
-        <v-tab :value="5">Contact</v-tab>
-      </v-tabs>
-    </v-card>
-
-    <!-- Remove max-width -->
-    <v-card class="mx-auto" color="rgba(200, 176, 153, 0.7)">
-      <v-row>
-        <v-col align-self="center">
-          <div class="d-flex justify-end">
-            <v-img
-              height="50"
-              max-width="50"
-              src="@/assets/location-no-bg.gif"
-              class=""
-            />
-            <div>
-              <div class="text-h3">
-                <h3>Location</h3>
-              </div>
-
-              <p class="ml-1 mb-10 mt-2 font-weight-bold">
-                Coffee bean location in the north of Thailand
-              </p>
-              <v-btn
-                color="rgb(140, 115, 70)"
-                height="25"
-                width="120"
-                variant="elevated"
-                rounded="xl"
-                >View All</v-btn
-              >
-            </div>
-          </div>
-        </v-col>
-
-        <v-col align-self="center">
-          <div class="justify-end">
-            <!-- Put new image below, then adjust image width-->
-            <v-img width="200" src="@/assets/logo.png" />
-          </div>
-        </v-col>
-      </v-row>
-    </v-card>
-    <div
-      class="d-flex flex-column fill-height justify-center align-center text-black"
-    >
-      <h1 class="text-h4 font-weight-thin mb-4">Vuetify</h1>
-      <h4 class="subheading">Build your application today!</h4>
-    </div>
-  </v-parallax>
+  <v-divider color="brown"> </v-divider>
+  <v-card flat>
+    <v-tabs v-model="tab" color="rgb(240, 200, 165)" align-tabs="center">
+      <v-tab :value="1">Home</v-tab>
+      <v-tab :value="2">Type coffee beans</v-tab>
+      <v-tab :value="3">Blog</v-tab>
+      <v-tab :value="4">News</v-tab>
+      <v-tab :value="5">Contact</v-tab>
+    </v-tabs>
+  </v-card>
+  <v-card flat>
+    <v-window v-model="tab">
+      <v-window-item :value="1"> <home /> </v-window-item>
+      
+    </v-window>
+  </v-card>
 </template>
 
 <script>
+import home from "../components/home.vue";
 export default {
   data: () => ({
-    tab: null,
+    tab: 1,
   }),
 };
 </script>
