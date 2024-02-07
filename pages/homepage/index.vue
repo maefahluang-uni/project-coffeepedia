c1c40cd0f5f74fe2eac20595816545ede12d00b2
BallZzi <6431503102@lamduan.mfu.ac.th>
2024-02-07 19:44:37 +0700

    adjust tabbar font weight

--- pages/homepage/index.vue
+++ pages/homepage/index.vue
@@ -2,28 +2,24 @@
   <v-divider color="brown"> </v-divider>
   <v-card flat>
     <v-tabs v-model="tab" color="rgb(240, 200, 165)" align-tabs="center">
-      <v-tab :value="1">Home</v-tab>
-      <v-tab :value="2">Type coffee beans</v-tab>
-      <v-tab :value="3">Blog</v-tab>
-      <v-tab :value="4">News</v-tab>
-      <v-tab :value="5">Contact</v-tab>
+      <v-tab :value="1" class="font-weight-bold">Home</v-tab>
+      <v-tab :value="2" class="font-weight-bold">Type coffee beans</v-tab>
+      <v-tab :value="3" class="font-weight-bold">Blog</v-tab>
+      <v-tab :value="4" class="font-weight-bold">News</v-tab>
+      <v-tab :value="5" class="font-weight-bold">Contact</v-tab>
     </v-tabs>
   </v-card>
   <v-card flat>
     <v-window v-model="tab">
       <v-window-item :value="1"> <home /> </v-window-item>
-      <v-window-item :value="2"><typepage />  </v-window-item>
-      <v-window-item :value="3"> <blog/> </v-window-item>
-      <v-window-item :value="4"> <news/> </v-window-item>
-      
-      <v-window-item
-        :value="5"
-        
-      >
-      <div class="d-flex justify-center align-center contact-window">
-        <contact />
-      </div>
-        
+      <v-window-item :value="2"><typepage /> </v-window-item>
+      <v-window-item :value="3"> <blog /> </v-window-item>
+      <v-window-item :value="4"> <news /> </v-window-item>
+
+      <v-window-item :value="5">
+        <div class="d-flex justify-center align-center contact-window">
+          <contact />
+        </div>
       </v-window-item>
     </v-window>
   </v-card>
