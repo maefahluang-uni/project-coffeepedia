08d035ab43a380be4242878f494f65089abce039
BallZzi <6431503102@lamduan.mfu.ac.th>
2024-02-08 16:05:48 +0700

    adjust background image

--- components/home.vue
+++ components/home.vue
@@ -101,114 +101,111 @@
         </div>
       </v-row>
     </v-card>
-
-    <v-row class="justify-center mb-15 mt-5">
-      <v-card color="white" flat width="175">
-        <h1 class="text-black blog-head">Our Blog</h1>
-      </v-card>
-      <v-divider color="brown" class="mt-n6 divider"></v-divider>
-    </v-row>
-    <div>
-      <v-col>
-        <v-row>
-          <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
-            <v-img src="@/assets/blog1.jpg" height="200px" cover></v-img>
-            <div>
-              <v-row class="mt-5 ml-3">
-                <v-icon icon="mdi-calendar" color="grey"></v-icon>
-                <p class="ml-2 text-grey">Nov 12 , 2023</p>
-                <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
-                <p class="ml-2 text-grey">6</p>
-                <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
-                <p class="ml-2 text-grey">76</p>
-              </v-row>
-            </div>
-            <v-card-title>
-              <b
-                >The difference between Arabica and Robusta coffee
-              </b></v-card-title
+  </div>
+  <v-row class="justify-center mb-15 mt-5">
+    <v-card color="white" flat width="175">
+      <h1 class="text-black blog-head">Our Blog</h1>
+    </v-card>
+    <v-divider color="brown" class="mt-n6 divider"></v-divider>
+  </v-row>
+  <div>
+    <v-col>
+      <v-row>
+        <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
+          <v-img src="@/assets/blog1.jpg" height="200px" cover></v-img>
+          <div>
+            <v-row class="mt-5 ml-3">
+              <v-icon icon="mdi-calendar" color="grey"></v-icon>
+              <p class="ml-2 text-grey">Nov 12 , 2023</p>
+              <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
+              <p class="ml-2 text-grey">6</p>
+              <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
+              <p class="ml-2 text-grey">76</p>
+            </v-row>
+          </div>
+          <v-card-title>
+            <b
+              >The difference between Arabica and Robusta coffee
+            </b></v-card-title
+          >
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="rgb(140, 115, 70)"
+              width="120"
+              height="25"
+              rounded="xl"
+              variant="elevated"
+              class="mb-3 mt-3"
             >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="rgb(140, 115, 70)"
-                width="120"
-                height="25"
-                rounded="xl"
-                variant="elevated"
-                class="mb-3 mt-3"
-              >
-                Read More
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
-            <v-img src="@/assets/blog2.jpg" height="200px" cover></v-img>
-            <div>
-              <v-row class="mt-5 ml-3">
-                <v-icon icon="mdi-calendar" color="grey"></v-icon>
-                <p class="ml-2 text-grey">June 20 , 2023</p>
-                <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
-                <p class="ml-2 text-grey">9</p>
-                <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
-                <p class="ml-2 text-grey">29</p>
-              </v-row>
-            </div>
-            <v-card-title>
-              <b
-                >What types of coffee beans are grown in each region of
-                Thailand?
-              </b></v-card-title
+              Read More
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
+          <v-img src="@/assets/blog2.jpg" height="200px" cover></v-img>
+          <div>
+            <v-row class="mt-5 ml-3">
+              <v-icon icon="mdi-calendar" color="grey"></v-icon>
+              <p class="ml-2 text-grey">June 20 , 2023</p>
+              <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
+              <p class="ml-2 text-grey">9</p>
+              <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
+              <p class="ml-2 text-grey">29</p>
+            </v-row>
+          </div>
+          <v-card-title>
+            <b
+              >What types of coffee beans are grown in each region of Thailand?
+            </b></v-card-title
+          >
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="rgb(140, 115, 70)"
+              width="120"
+              height="25"
+              rounded="xl"
+              variant="elevated"
+              class="mb-3 mt-3"
             >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="rgb(140, 115, 70)"
-                width="120"
-                height="25"
-                rounded="xl"
-                variant="elevated"
-                class="mb-3 mt-3"
-              >
-                Read More
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
-            <v-img src="@/assets/blog3.jpg" height="200px" cover></v-img>
-            <div>
-              <v-row class="mt-5 ml-3">
-                <v-icon icon="mdi-calendar" color="grey"></v-icon>
-                <p class="ml-2 text-grey">Sep 11 , 2022</p>
-                <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
-                <p class="ml-2 text-grey">1</p>
-                <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
-                <p class="ml-2 text-grey">11</p>
-              </v-row>
-            </div>
-            <v-card-title>
-              <b
-                >Get to know "Arabica", the ultimate in delicious coffee
-                varieties
-              </b></v-card-title
+              Read More
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="mx-auto" max-width="344" color="rgb(240, 240, 240)">
+          <v-img src="@/assets/blog3.jpg" height="200px" cover></v-img>
+          <div>
+            <v-row class="mt-5 ml-3">
+              <v-icon icon="mdi-calendar" color="grey"></v-icon>
+              <p class="ml-2 text-grey">Sep 11 , 2022</p>
+              <v-icon class="ml-3" icon="mdi-comment" color="grey"></v-icon>
+              <p class="ml-2 text-grey">1</p>
+              <v-icon class="ml-3" icon="mdi-eye" color="grey"></v-icon>
+              <p class="ml-2 text-grey">11</p>
+            </v-row>
+          </div>
+          <v-card-title>
+            <b
+              >Get to know "Arabica", the ultimate in delicious coffee varieties
+            </b></v-card-title
+          >
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="rgb(140, 115, 70)"
+              width="120"
+              height="25"
+              rounded="xl"
+              variant="elevated"
+              class="mb-3 mt-3"
             >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="rgb(140, 115, 70)"
-                width="120"
-                height="25"
-                rounded="xl"
-                variant="elevated"
-                class="mb-3 mt-3"
-              >
-                Read More
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-row>
-      </v-col>
-    </div>
+              Read More
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-col>
   </div>
 </template>
 <style scoped>
