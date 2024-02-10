6745f8d8a58fdc3e830959fc7535f785a5da09a2
nutthakanT <6431503086@lamduan.mfu.ac.th>
2024-02-10 16:52:31 +0700

    10feb

--- components/news.vue
+++ components/news.vue
@@ -1,3 +1,14 @@
 <template>
-    news
+    <v-img
+  :height="490"
+  aspect-ratio="16/9"
+  cover
+  src="@/assets/news_main.jpg">
+  <div class="d-flex flex-column fill-height justify-end align-start text-white ml-10">
+      <h1 class="mb-4">
+        What is the market situation for 'Thai coffee' at the beginning of 2023?
+      </h1>
+      <a href="https://www.thansettakij.com/sustainable/food-security/569628/" class="subheading mb-4 text-grey ">Read More</a>
+    </div>
+  </v-img>
 </template>
