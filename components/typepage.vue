ea71b5dba433f9e5bb212a395b184232a5993a3b
nutthakanT <6431503086@lamduan.mfu.ac.th>
2024-02-10 15:27:56 +0700

    10feb

--- components/typepage.vue
+++ components/typepage.vue
@@ -5,36 +5,16 @@
   <div>
     <v-layout style="height: 56px">
       <v-bottom-navigation v-model="value" color="teal">
-        <v-btn
-          rounded="xl"
-          color="rgb(140, 115, 70)"
-          height="30"
-          class="mt-5"
-        >
+        <v-btn rounded="xl" color="rgb(140, 115, 70)" height="30" class="mt-5">
           <h4>All</h4>
         </v-btn>
-        <v-btn
-          rounded="xl"
-          color="rgb(140, 115, 70)"
-          height="30"
-          class="mt-5"
-        >
+        <v-btn rounded="xl" color="rgb(140, 115, 70)" height="30" class="mt-5">
           <h4>Wet process</h4>
         </v-btn>
-        <v-btn
-          rounded="xl"
-          color="rgb(140, 115, 70)"
-          height="30"
-          class="mt-5"
-        >
+        <v-btn rounded="xl" color="rgb(140, 115, 70)" height="30" class="mt-5">
           <h4>Dry process</h4>
         </v-btn>
-        <v-btn
-          rounded="xl"
-          color="rgb(140, 115, 70)"
-          height="30"
-          class="mt-5"
-        >
+        <v-btn rounded="xl" color="rgb(140, 115, 70)" height="30" class="mt-5">
           <h4>Honey process</h4>
         </v-btn>
       </v-bottom-navigation>
@@ -43,141 +23,73 @@
   <div class="justify-space-between mb-6">
     <v-row>
       <p class="mt-10 ml-10">Showing All 9 Results</p>
-
-      <v-select
-        max-width="100"
-        class="mt-10 ml-5 mr-5"
-        label="Select"
-        :items="['Light-Dark', 'Dark-Light']"
-        variant="outlined"
-      ></v-select>
-
+    
     </v-row>
+
   </div>
 
-  <v-row class="justify-space-between ">
-    <v-hover v-slot="{ isHovering, props }">
-      <v-card
-        rounded="xl"
-        class="mx-auto mt-10"
-        color="rgba(185, 160, 130, 0.5)"
-        width="200"
-        height="300"
-        v-bind="props"
-      >
-        <v-img
-          cover
-          src="@/assets/wet_light.png"
-        >
-          <v-expand-transition>
-            <div
-              v-if="isHovering"
-              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
-              style="height: 100%"
-            >
-              <v-btn color="green" rounded="xl" width="100" height="30">
-                View
-              </v-btn>
-            </div>
-          </v-expand-transition>
-        </v-img>
-
-        <v-card-text class="pt-6">
-          <div class="text-h5 font-weight-drak text-brown mb-2">
-            Wet Process
-          </div>
-
-          <div class="font-weight-light text-black mb-2">Light Roasted</div>
-        </v-card-text>
-      </v-card>
-    </v-hover>
-
-    <v-hover v-slot="{ isHovering, props }">
-      <v-card
-        rounded="xl"
-        class="mx-auto mt-10"
-        color="rgba(185, 160, 130, 0.5)"
-        width="200"
-        height="300"
-        v-bind="props"
-      >
-        <v-img
-          cover
-          src="@/assets/wet_medium.png"
-        >
-          <v-expand-transition>
-            <div
-              v-if="isHovering"
-              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
-              style="height: 100%"
-            >
-              <v-btn color="green" rounded="xl" width="100" height="30">
-                View
-              </v-btn>
-            </div>
-          </v-expand-transition>
-        </v-img>
-
-        <v-card-text class="pt-6">
-          <div class="text-h5 font-weight-drak text-brown mb-2">
-            Wet Process
-          </div>
-
-          <div class="font-weight-light text-black mb-2">Medium Roasted</div>
-        </v-card-text>
-      </v-card>
-    </v-hover>
-
-    <v-hover v-slot="{ isHovering, props }">
-      <v-card
-        rounded="xl"
-        class="mx-auto mt-10"
-        color="rgba(185, 160, 130, 0.5)"
-        width="200"
-        height="300"
-        v-bind="props"
-      >
-        <v-img
-          cover
-          src="@/assets/wet_dark.png"
-        >
-          <v-expand-transition>
-            <div
-              v-if="isHovering"
-              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
-              style="height: 100%"
-            >
-              <v-btn color="green" rounded="xl" width="100" height="30">
-                View
-              </v-btn>
-            </div>
-          </v-expand-transition>
-        </v-img>
-        <v-card-text class="pt-6">
-          <div class="text-h5 font-weight-drak text-brown mb-2">
-            Wet Process
-          </div>
-
-          <div class="font-weight-light text-black mb-2">Dark Roasted</div>
-        </v-card-text>
-      </v-card>
-    </v-hover>
-  </v-row>
-  
   <v-row class="justify-space-between">
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card
+          rounded="xl"
+          class="mx-auto"
+          color="rgba(185, 160, 130, 0.5)"
+          width="200"
+          height="300"
+          v-bind="props"
+        >
+          <v-img cover src="@/assets/wet_light.png">
+            <v-expand-transition>
+              <div
+                v-if="isHovering"
+                class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
+                style="height: 100%"
+              >
+                <v-btn color="green" rounded="xl" width="100" height="30">
+                  View
+                </v-btn>
+              </div>
+            </v-expand-transition>
+          </v-img>
+
+          <v-card-text class="pt-6">
+            <div class="text-h5 font-weight-drak text-brown mb-2">
+              Wet Process
+            </div>
+
+            <div class="font-weight-light text-black mb-2">Light Roasted</div>
+          </v-card-text>
+        </v-card>
+      </v-hover>
+    </v-card>
+
+  <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
     <v-hover v-slot="{ isHovering, props }">
+      
       <v-card
         rounded="xl"
-        class="mx-auto mt-10"
+        class="mx-auto"
         color="rgba(185, 160, 130, 0.5)"
         width="200"
         height="300"
         v-bind="props"
       >
-        <v-img
-          cover
-          src="@/assets/dry_light.png"
-        >
+        <v-img cover src="@/assets/wet_medium.png">
           <v-expand-transition>
             <div
               v-if="isHovering"
@@ -193,63 +105,33 @@
 
         <v-card-text class="pt-6">
           <div class="text-h5 font-weight-drak text-brown mb-2">
-            Dry Process
-          </div>
-
-          <div class="font-weight-light text-black mb-2">Light Roasted</div>
-        </v-card-text>
-      </v-card>
-    </v-hover>
-
-    <v-hover v-slot="{ isHovering, props }">
-      <v-card
-        rounded="xl"
-        class="mx-auto mt-10"
-        color="rgba(185, 160, 130, 0.5)"
-        width="200"
-        height="300"
-        v-bind="props"
-      >
-        <v-img
-          cover
-          src="@/assets/dry_medium.png"
-        >
-          <v-expand-transition>
-            <div
-              v-if="isHovering"
-              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
-              style="height: 100%"
-            >
-              <v-btn color="green" rounded="xl" width="100" height="30">
-                View
-              </v-btn>
-            </div>
-          </v-expand-transition>
-        </v-img>
-
-        <v-card-text class="pt-6">
-          <div class="text-h5 font-weight-drak text-brown mb-2">
-            Dry Process
+            Wet Process
           </div>
 
           <div class="font-weight-light text-black mb-2">Medium Roasted</div>
         </v-card-text>
       </v-card>
     </v-hover>
+    </v-card>
 
+<v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
     <v-hover v-slot="{ isHovering, props }">
       <v-card
         rounded="xl"
-        class="mx-auto mt-10"
+        class="mx-auto"
         color="rgba(185, 160, 130, 0.5)"
         width="200"
         height="300"
         v-bind="props"
       >
-        <v-img
-          cover
-          src="@/assets/dry_dark.png"
-        >
+        <v-img cover src="@/assets/wet_dark.png">
           <v-expand-transition>
             <div
               v-if="isHovering"
@@ -262,33 +144,165 @@
             </div>
           </v-expand-transition>
         </v-img>
-
         <v-card-text class="pt-6">
           <div class="text-h5 font-weight-drak text-brown mb-2">
-            Dry Process
+            Wet Process
           </div>
 
           <div class="font-weight-light text-black mb-2">Dark Roasted</div>
         </v-card-text>
       </v-card>
     </v-hover>
+  </v-card>
   </v-row>
-  
 
   <v-row class="justify-space-between">
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
     <v-hover v-slot="{ isHovering, props }">
       <v-card
         rounded="xl"
-        class="mx-auto mt-10"
+        class="mx-auto"
         color="rgba(185, 160, 130, 0.5)"
         width="200"
         height="300"
         v-bind="props"
       >
-        <v-img
-          cover
-          src="@/assets/honey_light.png"
-        >
+        <v-img cover src="@/assets/dry_light.png">
+          <v-expand-transition>
+            <div
+              v-if="isHovering"
+              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
+              style="height: 100%"
+            >
+              <v-btn color="green" rounded="xl" width="100" height="30">
+                View
+              </v-btn>
+            </div>
+          </v-expand-transition>
+        </v-img>
+
+        <v-card-text class="pt-6">
+          <div class="text-h5 font-weight-drak text-brown mb-2">
+            Dry Process
+          </div>
+
+          <div class="font-weight-light text-black mb-2">Light Roasted</div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    </v-card>
+
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card
+        rounded="xl"
+        class="mx-auto"
+        color="rgba(185, 160, 130, 0.5)"
+        width="200"
+        height="300"
+        v-bind="props"
+      >
+        <v-img cover src="@/assets/dry_medium.png">
+          <v-expand-transition>
+            <div
+              v-if="isHovering"
+              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
+              style="height: 100%"
+            >
+              <v-btn color="green" rounded="xl" width="100" height="30">
+                View
+              </v-btn>
+            </div>
+          </v-expand-transition>
+        </v-img>
+
+        <v-card-text class="pt-6">
+          <div class="text-h5 font-weight-drak text-brown mb-2">
+            Dry Process
+          </div>
+
+          <div class="font-weight-light text-black mb-2">Medium Roasted</div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    </v-card>
+
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card
+        rounded="xl"
+        class="mx-auto"
+        color="rgba(185, 160, 130, 0.5)"
+        width="200"
+        height="300"
+        v-bind="props"
+      >
+        <v-img cover src="@/assets/dry_dark.png">
+          <v-expand-transition>
+            <div
+              v-if="isHovering"
+              class="d-flex transition-fast-in-fast-out bg-grey-darken-2 v-card--reveal"
+              style="height: 100%"
+            >
+              <v-btn color="green" rounded="xl" width="100" height="30">
+                View
+              </v-btn>
+            </div>
+          </v-expand-transition>
+        </v-img>
+
+        <v-card-text class="pt-6">
+          <div class="text-h5 font-weight-drak text-brown mb-2">
+            Dry Process
+          </div>
+
+          <div class="font-weight-light text-black mb-2">Dark Roasted</div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    </v-card>
+  </v-row>
+
+  <v-row class="justify-space-between">
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card
+        rounded="xl"
+        class="mx-auto"
+        color="rgba(185, 160, 130, 0.5)"
+        width="200"
+        height="300"
+        v-bind="props"
+      >
+        <v-img cover src="@/assets/honey_light.png">
           <v-expand-transition>
             <div
               v-if="isHovering"
@@ -311,20 +325,26 @@
         </v-card-text>
       </v-card>
     </v-hover>
+    </v-card>
 
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
     <v-hover v-slot="{ isHovering, props }">
       <v-card
         rounded="xl"
-        class="mx-auto mt-10"
+        class="mx-auto"
         color="rgba(185, 160, 130, 0.5)"
         width="200"
         height="300"
         v-bind="props"
       >
-        <v-img
-          cover
-          src="@/assets/honey_medium.png"
-        >
+        <v-img cover src="@/assets/honey_medium.png">
           <v-expand-transition>
             <div
               v-if="isHovering"
@@ -347,20 +367,26 @@
         </v-card-text>
       </v-card>
     </v-hover>
+    </v-card>
 
+    <v-card
+      variant="outlined"
+      rounded="xl"
+      class="mx-auto mt-10"
+      color="rgba(185, 160, 130)"
+      width="200"
+      height="300"
+    >  
     <v-hover v-slot="{ isHovering, props }">
       <v-card
         rounded="xl"
-        class="mx-auto mt-10"
+        class="mx-auto"
         color="rgba(185, 160, 130, 0.5)"
         width="200"
         height="300"
         v-bind="props"
       >
-        <v-img
-          cover
-          src="@/assets/honey_dark.png"
-        >
+        <v-img cover src="@/assets/honey_dark.png">
           <v-expand-transition>
             <div
               v-if="isHovering"
@@ -383,12 +409,13 @@
         </v-card-text>
       </v-card>
     </v-hover>
+    </v-card>
   </v-row>
 </template>
 
 <script>
 export default {
-  data: () => ({ value: 1 }),
+  data: () => ({ value: 1}),
 };
 </script>
 <style>
