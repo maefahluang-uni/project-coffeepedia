30d73c08223889cb0a5600e9a008841497a73b41
nutthakanT <6431503086@lamduan.mfu.ac.th>
2024-02-15 16:59:32 +0700

    15feb

--- components/news.vue
+++ components/news.vue
@@ -1,14 +1,123 @@
 <template>
-    <v-img
-  :height="490"
-  aspect-ratio="16/9"
-  cover
-  src="@/assets/news_main.jpg">
-  <div class="d-flex flex-column fill-height justify-end align-start text-white ml-10">
+  <v-img :height="490" aspect-ratio="16/9" cover src="@/assets/news_main.jpg">
+    <div
+      class="d-flex flex-column fill-height justify-end align-start text-white ml-10"
+    >
       <h1 class="mb-4">
         What is the market situation for 'Thai coffee' at the beginning of 2023?
       </h1>
-      <a href="https://www.thansettakij.com/sustainable/food-security/569628/" class="subheading mb-4 text-grey ">Read More</a>
+      <a
+        href="https://www.thansettakij.com/sustainable/food-security/569628/"
+        class="subheading mb-4 text-grey"
+        >Read More</a
+      >
     </div>
   </v-img>
+
+  <div class="d-flex flex-column align-start mt-10 ml-16">
+    <h1 class="text-brown">Lastest</h1>
+  </div>
+  <div class="mx-16 mt-5 mb-5"><v-divider></v-divider></div>
+  
+    <v-row class="d-flex justify-space-evenly">
+      <div>
+        <v-card
+          class="mx-auto my-12 "
+          max-width="300"
+          color="rgba(60, 60, 60, 0.2)">
+          <v-img cover height="300" src="@/assets/news_main.jpg"></v-img>
+
+          <v-card-item>
+            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-text>
+              <div class="text-h6">
+                Point out the direction in which the Thai coffee industry can
+                develop and be sustainable
+              </div>
+            </v-card-text>
+          </v-card-item>
+          <div>
+            <a
+              href="https://www.thairath.co.th/lifestyle/life/2709619"
+              class="subheading mb-4 text-grey d-flex justify-end align-end mr-5"
+              >Read More</a
+            >
+          </div>
+        </v-card>
+      </div>
+      <div>
+        <v-card
+          class="mx-auto my-12"
+          max-width="300"
+          color="rgba(60, 60, 60, 0.2)">
+          <v-img cover height="300" src="pic"></v-img>
+
+          <v-card-item>
+            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-text>
+              <div class="text-h6">
+                Revealing the reason why Thai people drink expensive 'coffee'
+              </div>
+            </v-card-text>
+          </v-card-item>
+          <div>
+            <a
+              href="https://www.dailynews.co.th/news/2053626/"
+              class="subheading mb-4 text-grey d-flex justify-end align-end mr-5"
+              >Read More</a
+            >
+          </div>
+        </v-card>
+      </div>
+    </v-row>
+    <v-row class="d-flex justify-space-evenly">
+      <div>
+        <v-card
+          class="mx-auto my-12 "
+          max-width="300"
+          color="rgba(60, 60, 60, 0.2)">
+          <v-img cover height="300" src="pic"></v-img>
+
+          <v-card-item>
+            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-text>
+              <div class="text-h6">
+                Thai people consume 70,000 tons of coffee per year, pushing the market value to reach 60 billion
+              </div>
+            </v-card-text>
+          </v-card-item>
+          <div>
+            <a
+              href="https://www.dailynews.co.th/news/2053626/"
+              class="subheading mb-4 text-grey d-flex justify-end align-end mr-5"
+              >Read More</a
+            >
+          </div>
+        </v-card>
+      </div>
+      <div>
+        <v-card
+          class="mx-auto my-12"
+          max-width="300"
+          color="rgba(60, 60, 60, 0.2)">
+          <v-img cover height="300" src="pic"></v-img>
+
+          <v-card-item>
+            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-text>
+              <div class="text-h6">
+                Get to know espresso A menu that is more than just black coffee.
+              </div>
+            </v-card-text>
+          </v-card-item>
+          <div>
+            <a
+              href="https://www.thairath.co.th/lifestyle/life/2290026"
+              class="subheading mb-4 text-grey d-flex justify-end align-end mr-5"
+              >Read More</a
+            >
+          </div>
+        </v-card>
+      </div>
+    </v-row>
 </template>
