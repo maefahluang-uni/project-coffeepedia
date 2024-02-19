a21c1303512870e620787da09d4a1bde9abab57f
nutthakanT <6431503086@lamduan.mfu.ac.th>
2024-02-19 17:01:54 +0700

    MockupNews

--- components/news.vue
+++ components/news.vue
@@ -22,10 +22,11 @@
     <v-row class="d-flex justify-space-evenly">
       <div>
         <v-card
-          class="mx-auto my-12 "
+          class="my-12"
           max-width="300"
+          max-height="600"
           color="rgba(60, 60, 60, 0.2)">
-          <v-img cover height="300" src="@/assets/news_main.jpg"></v-img>
+          <v-img cover height="300" src="@/assets/news1.jpg"></v-img>
 
           <v-card-item>
             <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
@@ -45,18 +46,19 @@
           </div>
         </v-card>
       </div>
+
       <div>
         <v-card
-          class="mx-auto my-12"
+          class="mx-auto my-12 "
           max-width="300"
           color="rgba(60, 60, 60, 0.2)">
-          <v-img cover height="300" src="pic"></v-img>
+          <v-img cover height="300" src="@/assets/news3.jpg"></v-img>
 
           <v-card-item>
-            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-subtitle class="ml-4"> July 11, 2023 </v-card-subtitle>
             <v-card-text>
               <div class="text-h6">
-                Revealing the reason why Thai people drink expensive 'coffee'
+                Thai people consume 70,000 tons of coffee per year, pushing the market value to reach 60 billion
               </div>
             </v-card-text>
           </v-card-item>
@@ -73,37 +75,40 @@
     <v-row class="d-flex justify-space-evenly">
       <div>
         <v-card
-          class="mx-auto my-12 "
+          class="my-12"
           max-width="300"
+          max-height="600"
           color="rgba(60, 60, 60, 0.2)">
-          <v-img cover height="300" src="pic"></v-img>
+          <v-img cover height="300" src="@/assets/news2.jpg"></v-img>
 
           <v-card-item>
-            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-subtitle class="ml-4"> Jan 15, 2023 </v-card-subtitle>
             <v-card-text>
               <div class="text-h6">
-                Thai people consume 70,000 tons of coffee per year, pushing the market value to reach 60 billion
+                Revealing the reason why Thai people drink expensive 'coffee'
               </div>
             </v-card-text>
           </v-card-item>
+          <v-spacer></v-spacer>
           <div>
             <a
-              href="https://www.dailynews.co.th/news/2053626/"
+              href="https://today.line.me/th/v2/article/kEZqkG1"
               class="subheading mb-4 text-grey d-flex justify-end align-end mr-5"
               >Read More</a
             >
           </div>
         </v-card>
       </div>
+
       <div>
         <v-card
           class="mx-auto my-12"
           max-width="300"
           color="rgba(60, 60, 60, 0.2)">
-          <v-img cover height="300" src="pic"></v-img>
+          <v-img cover height="300" src="@/assets/news4.jpg"></v-img>
 
           <v-card-item>
-            <v-card-subtitle class="ml-4"> July 15, 2023 </v-card-subtitle>
+            <v-card-subtitle class="ml-4"> Dec 10, 2022 </v-card-subtitle>
             <v-card-text>
               <div class="text-h6">
                 Get to know espresso A menu that is more than just black coffee.
